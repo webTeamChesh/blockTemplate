e919e9d3719ee0d221e21ebaefd542722f9bf4f6
antiloquax <tranter.m@sky.com>
2024-04-12 11:56:05 +0100

    Update ejsTemplates.js

--- app/ejsTemplates.js
+++ app/ejsTemplates.js
@@ -226,7 +226,7 @@ const appOuter = `
   <script type="importmap">
     {
       "imports": {
-        "vue": "https://unpkg.com/vue@3/dist/vue.esm-browser.prod.js"
+        "vue": "https://unpkg.com/vue@3.4.21/dist/vue.esm-browser.prod.js"
       }
     }
   </script>
